37b72871fe3bebb77b83cc8279d0832ecb8e430f
Igorot113 <121568339+Igorot113@users.noreply.github.com>
2023-01-03 18:25:02 -0300

    Create Desafio.go

---
+++ Desafio.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const ebulicaoK = 373
+
+func main(){
+	var tempC = (ebulicaoK - 273)
+	var tempK = 373
+	fmt.Printf ("Temperatura de ebulição da água em Kelvin para Celsius = %d e a temperatura de ebulição da água em Kelvin é =%d",tempC,tempK)
+}
